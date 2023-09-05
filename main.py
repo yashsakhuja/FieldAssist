0cc9b51083dba1b53a2f9b5929bf32113a6cea13
Yash Sakhuja <73343388+yashsakhuja@users.noreply.github.com>
2023-09-05 21:51:34 +0100

    main.py

--- main.py
+++ main.py
@@ -15,17 +15,11 @@ from io import BytesIO
 st.set_page_config(layout='wide')
 
 
-# # Inputting Squads& Team Sheets Data
-# github_excel_opp_url =  'https://raw.githubusercontent.com/yashsakhuja/FieldAssist/main/data/Opponent%20Team%20Sheet.csv'
-# # Fetch the Excel file from GitHub
-# data = rq.get(github_excel_opp_url).content
-# opponent_squad= pd.read_csv(BytesIO(data))
-
-
-# Inputting Squads& Team Sheets Data
+#Inputting Squads& Team Sheets Data
 github_excel_opp_url =  'https://raw.githubusercontent.com/yashsakhuja/FieldAssist/main/data/Opponent%20Team%20Sheet.csv'
 # Fetch the Excel file from GitHub
-opponent_squad= pd.read_csv(github_excel_opp_url)
+data = rq.get(github_excel_opp_url).content
+opponent_squad= pd.read_csv(BytesIO(data))
 
 
 github_excel_lancs_url =  "https://raw.githubusercontent.com/yashsakhuja/FieldAssist/main/data/Lancashire%20Team%20Sheet.csv"
