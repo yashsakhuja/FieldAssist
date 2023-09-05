acc1d655e4ced1752225aa55651aa1a61c51f920
Yash Sakhuja <73343388+yashsakhuja@users.noreply.github.com>
2023-09-05 21:54:08 +0100

    main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.set_page_config(layout='wide')
 
 
 #Inputting Squads& Team Sheets Data
-github_excel_opp_url =  'https://raw.githubusercontent.com/yashsakhuja/FieldAssist/main/data/Opponent%20Team%20Sheet.csv'
+github_excel_opp_url =  'https://github.com/yashsakhuja/FieldAssist/blob/main/data/Opponent%20Team%20Sheet.csv'
 # Fetch the Excel file from GitHub
 data = rq.get(github_excel_opp_url).content
 opponent_squad= pd.read_csv(BytesIO(data))
