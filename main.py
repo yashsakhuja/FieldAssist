1e80d506cd46f109f94c4e8472af427b78019eed
Yash Sakhuja <73343388+yashsakhuja@users.noreply.github.com>
2023-09-05 16:02:29 +0100

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-
-import gspread_pandas
-from gspread_pandas import Spread,Client
+from gspread-pandas import Spread,Client
 from google.oauth2 import service_account
 
 
