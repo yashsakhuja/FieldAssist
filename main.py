fd0f0ca5a887a03f54e9f9860312ae7b48e4fae6
Yash Sakhuja <73343388+yashsakhuja@users.noreply.github.com>
2023-09-06 01:47:48 +0100

    main.py

--- main.py
+++ main.py
@@ -520,7 +520,7 @@ def update_the_spreadsheet(url:object,spreadsheetname: object, dataframe: object
                                   'Relay_Y/N', 'Relay_Player', 'Relay_Type', 'Relay_Activity', 'Stumping_Activity','Dismissal']
             sh = client.open_by_url(url)
             worksheet = sh.worksheet(spreadsheetname)
-            worksheet.append_row(list(dataframe[col]))
+            worksheet.append_rows(list(dataframe))
 
 def update_the_spreadsheet_del(url:str,spreadsheetname: object, dataframe: object) -> object:
             col = ['Over', 'Ball', 'Extra_Y/N', 'Extra_Wide', 'Extra_Byes', 'Extra_LegByes',
